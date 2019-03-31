934767ba9cd57edb21a9a28bd8592f47b545c59d
ethan.hurst <ethan@hursty.co.nz>
2019-04-01 11:05:04 +1300

    Database working correctly

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const key = require("./config/keys.js");
 const express = require('express');
 const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose.connect(key.url, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log("Database Connected!"))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('What are you looking at'));
 
