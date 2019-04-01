4a87dbbabcddae4f63b9c4f5118ae035b0d6f429
ethan.hurst <ethan@hursty.co.nz>
2019-04-01 16:46:55 +1300

    More work on profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -24,12 +24,17 @@ router.get('/test', (req, res) => {
 router.get('/', passport.authenticate('jwt', {
   session: false
 }), (req, res) => {
+  const errors = {}
   Profile.findOne({
     user: req.user.id
   }).then(profile => {
     if (!profile) {
-      return res.status(404).json({})
+      errors.noprofile = 'There is no profile for this user!'
+      return res.status(404).json(errors)
     }
+    res.json(profile)
+  }).catch(err => {
+    res.status(404).json(err)
   })
 })
 
