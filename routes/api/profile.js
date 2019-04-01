2f26da7bc8e0513971d19b19509ce692a235d922
ethan.hurst <ethan@hursty.co.nz>
2019-04-01 16:57:03 +1300

    More work to the profile creation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -18,6 +18,7 @@ router.get('/test', (req, res) => {
 })
 
 /**
+ * @ROUTE GET api/profile
  *@desc Get Current logged in users profile
  *@Access Protected
  */
@@ -36,6 +37,37 @@ router.get('/', passport.authenticate('jwt', {
   }).catch(err => {
     res.status(404).json(err)
   })
+})
+
+/**
+ * @ROUTE POST api/profile
+ *@desc Create or EDIT a user profile
+ *@Access Protected
+ */
+router.post('/', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  const profileFields = {}
+  profileFields.user = req.user.id
+  if (req.body.handle) profileFields.handle = req.body.handle
+  if (req.body.company) profileFields.company = req.body.company
+  if (req.body.website) profileFields.website = req.body.website
+  if (req.body.location) profileFields.location = req.body.location
+  if (req.body.status) profileFields.status = req.body.status
+  if (req.body.interests) profileFields.interests = req.body.interests
+  if (req.body.bio) profileFields.bio = req.body.bio
+  if (req.body.instagramUsername) profileFields.instagramUsername = req.body.instagramUsername
+  if (typeof req.body.experience !== 'undefined') {
+    profileFields.experience = req.body.experience.split(',')
+  }
+  profileFields.social = {}
+  if (req.body.youtube) profileFields.social.youtube = req.body.youtube
+  if (req.body.twitter) profileFields.social.twitter = req.body.twitter
+  if (req.body.facebook) profileFields.social.facebook = req.body.facebook
+  if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin
+  if (req.body.instagram) profileFields.social.instagram = req.body.instagram
+
+  if (req.body.date) profileFields.date = req.body.date
 })
 
 module.exports = router
