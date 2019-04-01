599863911c5016710d526842c98b914f74796d4f
ethan.hurst <ethan@hursty.co.nz>
2019-04-01 13:26:55 +1300

    added json web tokenization

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../../config/keys')
 
 const User = require('../../models/User')
 
@@ -62,7 +64,6 @@ router.post('/register', (req, res) => {
  * @access Public
  */
 router.post('/login', (req, res) => {
-  // eslint-disable-next-line no-unused-vars
   const email = req.body.email
   const password = req.body.password
   User.findOne({
@@ -70,19 +71,31 @@ router.post('/login', (req, res) => {
   }).then(user => {
     if (!user) {
       return res.status(404).json({
-        email: 'Email or Password incorrect'
+        email: 'Email incorrect'
       })
     }
     bcrypt.compare(password, user.password)
       .then(isMatch => {
         if (isMatch) {
-          res.json({
-            msg: 'Success!'
+          const payload = {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar
+          }
+          jwt.sign(payload, keys.secret, {
+            expiresIn: '2h'
+          }, (err, token) => {
+            if (err) throw err
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            })
+          })
+        } else {
+          return res.status(400).json({
+            password: 'Password incorrect'
           })
         }
-        return res.status(400).json({
-          password: 'Email or Password incorrect'
-        })
       })
       .catch()
   }).catch()
