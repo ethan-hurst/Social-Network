37df93d9656134279c27d6063f5c53b39266e6e1
ethan.hurst <ethan@hursty.co.nz>
2019-04-02 08:37:45 +1300

    added in two additional GET methods for fetching profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -44,6 +44,50 @@ router.get('/', passport.authenticate('jwt', {
 })
 
 /**
+ * @ROUTE GET api/profile/:handle
+ *@desc Get Profile by Handle
+ *@Access Private
+ */
+router.get('/:handle', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  const errors = {}
+  Profile.findOne({
+    handle: req.params.handle
+  }).populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no Profile for this user'
+        res.status(404).json(errors)
+      }
+      res.json(profile)
+    })
+    .catch(err => res.status(404).json(err))
+})
+
+/**
+ * @ROUTE GET api/profile/:user_id
+ *@desc Get Profile by user ID
+ *@Access Private
+ */
+router.get('/user/:user_id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  const errors = {}
+  Profile.findOne({
+    user: req.params.user_id
+  }).populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no Profile for this user'
+        res.status(404).json(errors)
+      }
+      res.json(profile)
+    })
+    .catch(err => res.status(404).json(err))
+})
+
+/**
  * @ROUTE POST api/profile
  *@desc Create or EDIT a user profile
  *@Access Protected
