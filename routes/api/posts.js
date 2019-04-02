e0bccca139112aeca607624076a22e48341e6615
ethan.hurst <ethan@hursty.co.nz>
2019-04-03 10:37:13 +1300

    Added delete Route for comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -184,13 +184,79 @@ router.delete('/:id', passport.authenticate('jwt', {
 router.post('/comment/:id', passport.authenticate('jwt', {
   session: false
 }), (req, res) => {
+  const {
+    errors,
+    isValid
+  } = validatePostInput(req.body)
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
   Post.findById(req.params.id)
     .then(post => {
       const newComment = {
         text: req.body.text,
-        name: req.body.name
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
       }
+      post.comments.unshift(newComment)
+      post.save()
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({
+          err,
+          postnotfound: 'no post found!'
+        }))
     })
+})
+
+/**
+ * DELETE Route for /api/posts/comment/:id/:comment_id
+ * @desc Delete Comments on a Post
+ * @access Private
+ */
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+        return res.status(404).json({
+          nocommentexists: 'No comment exists!'
+        })
+      }
+      const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id)
+      post.comments.splice(removeIndex, 1)
+      post.save()
+        .then(post => res.json(post))
+    }).catch(err => res.status(404).json({
+      err,
+      postnotfound: 'no post found!'
+    }))
+})
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', {
+  session: false
+}), (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      // Check to see if comment exists
+      if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+        return res.status(404).json({
+          commentnotexists: 'Comment does not exist'
+        })
+      }
+      const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id)
+      post.comments.splice(removeIndex, 1)
+      post.save()
+        .then(post => res.json(post))
+    })
+    .catch(err => res.status(404).json({
+      err,
+      postnotfound: 'No post found'
+    }))
 })
 
 module.exports = router
