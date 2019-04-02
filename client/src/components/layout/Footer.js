49c4b2ef8e3cae90560f6b600fecccf8aebfc77c
ethan.hurst <ethan@hursty.co.nz>
2019-04-03 11:52:45 +1300

    added in html

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer class="bg-dark text-white mt-5 p-4 text-center">
+          Copyright &copy; 2019 Just Friends
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Footer
