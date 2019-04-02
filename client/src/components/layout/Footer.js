5df9bb2efc08c76d03e3b3b5d84c4795f7f9bb1a
ethan.hurst <ethan@hursty.co.nz>
2019-04-03 11:53:59 +1300

    changed footer to be functional component

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Footer extends Component {
-  render() {
-    return (
-      <div>
-        <footer class="bg-dark text-white mt-5 p-4 text-center">
-          Copyright &copy; 2019 Just Friends
-        </footer>
-      </div>
-    )
-  }
+export default function Footer () {
+  return (
+    <div>
+      <footer class="bg-dark text-white mt-5 p-4 text-center">
+        Copyright &copy; 2019 Just Friends
+      </footer>
+    </div >
+  )
 }
-
-export default Footer
