4d7e720f90b52ca0a8886c01daf7e7db4222cab2
ethan.hurst <ethan@hursty.co.nz>
2019-04-04 12:33:21 +1300

    Redux boiler plate

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,26 @@ import React, {
 } from 'react';
 import './custom.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 import { Navbar, Footer, Landing } from './components/layout';
 import { Login, Register } from './components/auth';
+import store from './store';
 
 class App extends Component {
   render() {
     return (
       <>
-        <Router>
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-          </div>
-          <Footer />
-        </Router>
+        <Provider store={store}>
+          <Router>
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
+          </Router>
+        </Provider>
       </>
     );
   }
