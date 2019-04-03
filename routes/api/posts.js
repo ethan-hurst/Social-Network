9fc1db80fd0eebda1e980cdf43a34187452e871f
ethan.hurst <ethan@hursty.co.nz>
2019-04-04 09:22:43 +1300

    fixed likes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -41,35 +41,26 @@ router.get('/', (req, res) => {
  * @desc Like Posts
  * @access Private
  */
-router.post('/like/:id', passport.authenticate('jwt', {
-  session: false
-}), (req, res) => {
-  Profile.findOne({
-    user: req.user.id
-  }).then(profile => {
-    Post.findById(req.params.id)
-      .then(post => {
-        if (
-          post.likes.filter(like => like.user.toString() === req.user.id)
-            .length > 0
-        ) {
-          return res
-            .status(400)
-            .json({
-              alreadyliked: 'User already liked this post'
-            })
-        }
-        post.likes.unshift({
-          user: req.user.id
+router.post('/like/:id', passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyliked: 'User already liked this post' });
+          }
+          post.likes.unshift({ user: req.user.id });
+          post.save().then(post => res.json(post));
         })
-        post.save().then(post => res.json(post))
-      })
-      .catch(err => res.status(404).json({
-        err,
-        postnotfound: 'No post found'
-      }))
-  })
-})
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    });
+  }
+);
 
 /**
  * POST Route for /api/posts/unlike/:id
