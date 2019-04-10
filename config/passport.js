8ff70d0350011c153c39d2ec41be5654672b8556
ethan.hurst <ethan@hursty.co.nz>
2019-04-10 13:46:35 +1200

    change to git ignore

---
+++ config/passport.js
@@ -0,0 +1,23 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+const mongoose = require('mongoose')
+const User = mongoose.model('users')
+const keys = require('./keys')
+
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+opts.secretOrKey = keys.secret
+
+module.exports = passport => {
+  // eslint-disable-next-line camelcase
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    User.findById(jwt_payload.id)
+      .then(user => {
+        if (user) {
+          return done(null, user)
+        }
+        return (null, false)
+      })
+      .catch(err => console.log(err))
+  }))
+}
