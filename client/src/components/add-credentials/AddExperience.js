625007409575ee1e8ea5f7849c523edbc0b501df
ethan.hurst <ethan@hursty.co.nz>
2019-04-12 11:00:06 +1200

    updated homepage

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unused-prop-types */
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
