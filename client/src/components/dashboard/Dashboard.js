9c148c01a93ee1ec3304eee16184fb223ef394ab
ethan.hurst <ethan@hursty.co.nz>
2019-04-12 15:03:13 +1200

    finished education

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
+import Experience from './Experience';
+import Education from './Education';
 
 class Dashboard extends Component {
   componentDidMount() {
@@ -30,6 +32,8 @@ class Dashboard extends Component {
             <Link to={`/profile/${profile.handle}`}>{user.name}</Link>
           </p>
           <ProfileActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
           <div style={{ marginBottom: '60px' }}>
             <button type="button" className="btn btn-danger" onClick={this.onDeleteClick.bind(this)}>Delete My Account</button>
           </div>
